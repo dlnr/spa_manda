0ab961e7c82f5675feaecd7160f0e853c5e47bb3
dlnr <niels@dlnr.org>
2015-04-13 09:50:48 +0200

    bezig met nav menu

--- wp-content/themes/spamanda/functions.php
+++ wp-content/themes/spamanda/functions.php
@@ -3,7 +3,7 @@
 function spamanda_scripts() {
   wp_enqueue_style( 'style-bootstrap', get_template_directory_uri() . '/css/bootstrap.css', array(), '1.0.0', false );
   wp_enqueue_style( 'style-bootstrap-responsive', get_template_directory_uri() . '/css/bootstrap-responsive.css', array(), '1.0.0', false );
-	wp_enqueue_style( 'style-spamanda', get_template_directory_uri() . '/css/style.css', array(), '1.0.0', false );
+	wp_enqueue_style( 'style-spamanda', get_template_directory_uri() . '/css/style.css', array(), '1.0.1', false );
   wp_enqueue_style( 'style-media', get_template_directory_uri() . '/css/media.css', array(), '1.0.0', false );
   wp_enqueue_style( 'style-camera', get_template_directory_uri() . '/css/camera.css', array(), '1.0.0', false );
   wp_enqueue_script( 'jquery' );
--- wp-content/themes/spamanda/header.php
+++ wp-content/themes/spamanda/header.php
@@ -4,7 +4,6 @@
   <head>
     <meta charset="utf-8">
     <title><?php wp_title(); ?></title>
-
     <meta name="viewport" content="width=device-width,  initial-scale=1, maximum-scale=1">
 
   <!--[if lt IE 9]>
@@ -12,13 +11,6 @@
   <![endif]-->
 
   <link href='http://fonts.googleapis.com/css?family=Rouge+Script|Open+Sans|Oswald:400,300' rel='stylesheet' type='text/css'>
-
-
-<!-- <link href="css/bootstrap.css" rel="stylesheet">
-<link href="css/bootstrap-responsive.css" rel="stylesheet">
-<link href="css/style.css" rel="stylesheet">
-<link href="css/media.css" rel="stylesheet">
-<link rel="stylesheet" media="screen" href="css/camera.css" /> -->
 
     <?php wp_head(); ?>
 
@@ -32,7 +24,7 @@
 
 
   <div class="phone">
-    <p>Contact Phone: +99 861 440 210</p>
+    <p>Bel: <a href="tel:0235768670">023 576 8670</a></p>
   </div>
 
 
@@ -59,63 +51,4 @@
     </div>
   </header>
 
-
-
-        <div class="camera_wrap" id="camera_wrap">
-
-             <div data-thumb="<?php echo get_template_directory_uri() ?>/images/p5.png"  data-src="<?php echo get_template_directory_uri() ?>/images/1.png" data-time="1500" data-trasPeriod="3000" data-target="_blank">
-                  <div class="fadeIn cameracaption1">
-                  <h2>spa and wellness</h2>
-                   <h3>premium website template</h3>
-  <p>
-  Lorem ipsum dolor sit amet, consectetuer adipiscing elit<span> sed diam nonummy </span>nibh euismod tincidunt ut laoreet dolore magna aliquam.
-  </p>
-
-  <section class="buttoncameraslider">
-  <a href="">read more</a>
-  </section>
-
-
-            </div>
-              </div>
-
-  <div data-thumb="<?php echo get_template_directory_uri() ?>/images/p3.png" data-src="<?php echo get_template_directory_uri() ?>/images/1.png" data-time="1500" data-trasPeriod="3000" data-target="_blank">
-                  <div class="fadeIn cameracaption1">
-
-                  <h2>spa and wellness</h2>
-                   <h3>premium website template</h3>
-  <p>
-  Lorem ipsum dolor sit amet, consectetuer adipiscing elit<span> sed diam nonummy </span>nibh euismod tincidunt ut laoreet dolore magna aliquam.
-  </p>
-
-  <section class="buttoncameraslider">
-  <a href="">read more</a>
-  </section>
-
-                  </div>
-              </div>
-
-
-
-             <div data-thumb="<?php echo get_template_directory_uri() ?>/images/p3.png"  data-src="<?php echo get_template_directory_uri() ?>/images/1.png" data-time="1500" data-trasPeriod="3000" data-target="_blank">
-                  <div class="fadeIn cameracaption1">
-
-                  <h2>spa and wellness</h2>
-                   <h3>premium website template</h3>
-  <p>
-  Lorem ipsum dolor sit amet, consectetuer adipiscing elit<span> sed diam nonummy </span>nibh euismod tincidunt ut laoreet dolore magna aliquam.
-  </p>
-
-  <section class="buttoncameraslider">
-  <a href="">read more</a>
-  </section>
-
-
-
-                 </div>
-              </div>
-
-
-          </div><!-- #camera_wrap -->
-
-  </div>
+</div>
