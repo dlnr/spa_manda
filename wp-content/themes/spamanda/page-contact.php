e036e40ee4bf576e1912ab420dc9d5bfff164dde
dlnr <niels@dlnr.org>
2015-06-13 23:11:58 +0200

    contact

---
+++ wp-content/themes/spamanda/page-contact.php
@@ -0,0 +1,170 @@
+<?php
+
+get_header();
+
+?>
+
+<div id="Contact">
+    <div class="underslider2">
+
+        <div class="container">
+            <div class="row">
+                <div class="span6">
+
+                    <h2>contact<span></span></h2>
+
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+
+
+    <div class="container">
+        <div class="row">
+            <div class=" borderbottom span12">
+
+                <div class="row">
+                    <div class="span8">
+                        <!--left-->
+
+
+                        <div class="">
+
+
+                            <div class="columnheadlineabout">
+                                <h4><span>Neem contact met ons op</span></h4>
+                            </div>
+
+
+                            <p>
+                                Bedankt voor uw interesse in Spa Manda. U kunt ons altijd bereiken voor vragen of reserveringen. Via e-mail, telefoon, of zelfs Facebook kunt u ons uw vraag stellen of reservering maken. Hoeronder kunt u een bericht achterlaten, en vind u een kaart van onze locatie in Haarlem. Aan de rechterkant van deze pagina vindt u onze contact gegevens en telefoonnummer.
+                            </p>
+
+
+
+
+                            <!--/comment-form-->
+                            <div>
+
+                                <h4 class="sendmessage">Verstuur bericht</h4>
+
+                                <div>
+
+                                    <?php the_content(); ?>
+
+
+                                </div>
+                            </div>
+
+
+
+                        </div>
+                        <!--leftabout end-->
+
+
+                    </div>
+                    <!--span8 left end-->
+
+
+                    <aside class="span4">
+                        <!--right-->
+
+
+
+                        <div class="headlinesidebar">
+                            <h4><span>Contact informatie</span></h4>
+                        </div>
+
+                        <div class="sidebarabout3">
+
+                            <ul>
+
+                                <li>
+                                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/iconaddress.png" alt="">
+                                    <p>Zijlstraat 69
+                                        <br>2011 TL Haarlem</p>
+                                </li>
+
+                                <li>
+                                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/iconphone.png" alt="">
+                                    <p>023-5768670</p>
+                                </li>
+
+                                <li>
+                                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/iconmail.png" alt="">
+
+                                    <a href="mailto:info@smapanda.nl?Subject=contact">
+                                        <p class="highlight">info@spamanda.nl</p>
+                                    </a>
+
+                                </li>
+
+                            </ul>
+
+
+
+                            <div class="reservation">
+                                <a href="">Telefoon:<span>023-5768670</span></a>
+                            </div>
+
+                        </div>
+                        <!--/sidebarabout3 end-->
+
+
+                        <div class="headlinesidebar">
+                            <h4><span>Openingstijden:</span></h4>
+                        </div>
+
+
+                        <ul class="sidebarcontact">
+                            <li>Maandag    <span> 12:00-19:00</span></li>
+                            <li>Dinsdag    <span> 10:00-19:00</span></li>
+                            <li>Woensdag   <span> 10:00-19:00</span></li>
+                            <li>Donderdag  <span> 10:00-20:00</span></li>
+                            <li>Vrijdag    <span> 10:00-19:00</span></li>
+                            <li>Zaterdag   <span> 10:00-19:00</span></li>
+                            <li>Zondag     <span> gesloten </span></li>
+                        </ul>
+
+
+
+                    </aside>
+                    <!--/span4 rightend-->
+
+
+
+
+                </div>
+                <!--/row end-->
+            </div>
+            <!--/borderbottom end-->
+        </div>
+        <!--/row end-->
+
+
+        <div class="row">
+            <div class="span12">
+
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2435.3497910505725!2d4.633825!3d52.382209!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5ef6adffb4b3b%3A0x8d4874aa439e8bc4!2sSpa+Manda!5e0!3m2!1sen!2snl!4v1415457034780" width="1280" height="500" frameborder="0" style="border:0"></iframe>
+
+                <br /><small><a href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;q=New+York&amp;aq=&amp;sll=37.0625,-95.677068&amp;sspn=36.505383,56.513672&amp;ie=UTF8&amp;geocode=FXFAbQIdK8KW-w&amp;split=0&amp;hq=&amp;hnear=New+York&amp;t=m&amp;z=10&amp;ll=40.714353,-74.005973" >View Larger Map</a></small>
+
+            </div>
+            <!--/span12 end-->
+        </div>
+        <!--/row end-->
+
+
+
+    </div>
+    <!--/container end-->
+</div>
+
+<?php
+
+get_footer();
+
+?>
